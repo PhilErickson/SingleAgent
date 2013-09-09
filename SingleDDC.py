5fe5ff99e8316780937f45beeb3fc96c7117c7ac
Philip J. Erickson <erickson.philip.j@gmail.com>
2013-09-09 14:56:10 -0500

    Initial commit - Front matter and imports

---
+++ SingleDDC.py
@@ -0,0 +1,11 @@
+'''
+    File        : SingleDDC.py
+    Author      : Philip J. Erickson
+    Date        : September 9, 2013
+    Description : Code for estimating Single Agent DDC models using:
+                    - Rust NFP algorithm (Rust 87)
+                    - Hotz and Miller CCP method (Hotz and Miller 93)
+'''
+
+import pandas as pd
+import numpy as np
